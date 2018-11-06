31545cea4a97c146d7080eb0db3d05ae70299127
Varun Sikka <sikkavarun@gmail.com>
2018-11-06 01:23:12 -0500

    added the commands to update the state of the led lights

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ module.exports = function(sails) {
     
   const setup = require('./lib/setup.js');
   const exec = require('./lib/exec');
-  
+  const commands = require('./lib/commands');
   return {
     setup,
-    exec
+    exec,
+    commands
   };
 };
